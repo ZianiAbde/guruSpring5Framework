2e8011e2bbbc466df26967a4be2c5364d9689376
ZianiAbde <abdelkader.ziani@outlook.com>
2019-11-28 09:15:43 -0500

    repositories interfaces

--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -20,6 +20,7 @@ public class Book {
             inverseJoinColumns =@JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
 
+
     public Book(String title) {
         this.title = title;
     }
